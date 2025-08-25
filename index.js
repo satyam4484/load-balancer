89a3c5fb5213a43bac989ae0233be97b1b82d228
satyam4484 <satyambsingh93@gmail.com>
2025-08-25 20:46:20 +0530

    changes

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js running on EC2 🚀");
+    res.send("Hello from Node.js running on EC2 🚀");
+});
+
+app.get("/heavy", (req, res) => {
+    let sum = 0;
+    for (let i = 0; i < 1e8; i++) {
+        sum += i;
+    }
+    res.send(`Heavy computation done! Sum: ${sum}`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
